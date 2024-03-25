09abb09237bd5806e0b55efccf0ce2127ab5fb88
Marth Karyn <marthkar@mi-f5.msk.21-school.ru>
2024-03-25 19:06:46 +0300

    upd

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -1,5 +1,19 @@
 #include "s21_decimal.h"
 
-int s21_decimal() {
-    
+void print_binary(int num) {
+    for (int i = 31; i >= 0; i--) {
+        printf("%d", (num >> i) & 1);
+    }
+}
+
+void print_decimal(s21_decimal num) {
+    for (int i = 0; i <= 3; i++) {
+        print_binary(num.bits[i]);
+    }
+}
+
+int main() {
+    s21_decimal num = {0, 0, 0, 2147483649};
+    print_decimal(num);
+    return 0;        
 }
