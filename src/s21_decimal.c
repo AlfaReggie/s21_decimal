a9d7cfa5a4519d8c8dba7d7fac03f01daa6edd21
Marth Karyn <marthkar@mi-f5.msk.21-school.ru>
2024-03-28 14:55:32 +0300

    update

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -7,13 +7,27 @@ void print_binary(int num) {
 }
 
 void print_decimal(s21_decimal num) {
-    for (int i = 0; i <= 3; i++) {
+    for (int i = 3; i >= 0; i--) {
         print_binary(num.bits[i]);
+        printf(" ");
     }
 }
 
+void set_sign(s21_decimal* num, int sign) {
+    num->bits[3] |= sign << 31;
+}
+
+void set_scale(s21_decimal* num, int scale) {
+    num->bits[3] |= scale << 16;
+}
+
 int main() {
-    s21_decimal num = {0, 0, 0, 2147483649};
-    print_decimal(num);
+    s21_decimal num2 = {1, 256, 0, 0};
+    set_sign(&num2, 0);
+    set_scale(&num2, 3);
+    print_decimal(num2);    
+    for (int i = 0; i < 4; i++) {
+        printf("%d ", num2.bits[i]);
+    }
     return 0;        
 }
